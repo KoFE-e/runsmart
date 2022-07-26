d63cfbeab13a6a2327436ffd0a12686fa5206bf5
KoFE <petrovykh03@gmail.com>
2022-07-26 23:26:16 +0500

    first section

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,13 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
                 prefix: "",
                 suffix: ".min",
               }))
             .pipe(autoprefixer({
-                browsers: ['last 2 versions'],
                 cascade: false
             }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
@@ -31,7 +30,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
